61af7ad657c1e22400d2524aefb40ef49ad99396
ist4284439 <ist@sunhitech.vn>
2024-07-01 17:02:50 +0700

    first add

---
+++ namecard/namecard/Scripts/scrollAnimation.js
@@ -0,0 +1,19 @@
+﻿<script>
+    document.addEventListener("DOMContentLoaded", function () {
+        const sections = document.querySelectorAll(".hidden");
+
+    function checkScroll() {
+        sections.forEach(section => {
+            const sectionTop = section.getBoundingClientRect().top;
+            const windowHeight = window.innerHeight;
+
+            if (sectionTop < windowHeight * 0.75) {
+                section.classList.add("visible");
+            }
+        });
+        }
+
+    window.addEventListener("scroll", checkScroll);
+    checkScroll(); // Check initial state
+    });
+</script>
